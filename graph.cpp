7e87fdb9e3019517bcc1ebdc0f0c18a54bf4a328
samarth <123548588+sam-3115@users.noreply.github.com>
2026-04-22 09:38:32 +0530

    Implement graph class with BFS, DFS, and Dijkstra

---
+++ graph.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph {
+    int V;
+    vector<vector<pair<int,int>>> adj;
+
+public:
+    Graph(int V) {
+        this->V = V;
+        adj.resize(V);
+    }
+
+    void addEdge(int u, int v, int w = 1) {
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    // BFS
+    vector<int> bfs(int start) {
+        vector<int> result;
+        vector<bool> visited(V, false);
+        queue<int> q;
+
+        q.push(start);
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            result.push_back(node);
+
+            for (auto &nbr : adj[node]) {
+                if (!visited[nbr.first]) {
+                    visited[nbr.first] = true;
+                    q.push(nbr.first);
+                }
+            }
+        }
+        return result;
+    }
+
+    // DFS
+    void dfsUtil(int node, vector<bool>& visited, vector<int>& result) {
+        visited[node] = true;
+        result.push_back(node);
+
+        for (auto &nbr : adj[node]) {
+            if (!visited[nbr.first]) {
+                dfsUtil(nbr.first, visited, result);
+            }
+        }
+    }
+
+    vector<int> dfs(int start) {
+        vector<int> result;
+        vector<bool> visited(V, false);
+        dfsUtil(start, visited, result);
+        return result;
+    }
+
+    // Dijkstra
+    vector<int> dijkstra(int start) {
+        vector<int> dist(V, INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
+        dist[start] = 0;
+        pq.push({0, start});
+
+        while (!pq.empty()) {
+            auto [d, node] = pq.top();
+            pq.pop();
+
+            for (auto &nbr : adj[node]) {
+                int next = nbr.first;
+                int weight = nbr.second;
+
+                if (dist[node] + weight < dist[next]) {
+                    dist[next] = dist[node] + weight;
+                    pq.push({dist[next], next});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+int main() {
+    Graph g(5);
+
+    g.addEdge(0,1,4);
+    g.addEdge(0,2,2);
+    g.addEdge(1,3,5);
+    g.addEdge(2,4,3);
+
+    vector<int> bfsRes = g.bfs(0);
+    cout << "BFS: ";
+    for (int x : bfsRes) cout << x << " ";
+    cout << endl;
+
+    vector<int> dfsRes = g.dfs(0);
+    cout << "DFS: ";
+    for (int x : dfsRes) cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
