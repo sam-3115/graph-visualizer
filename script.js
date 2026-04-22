1cc7b9493f01d8ab201ccc70936eaaff0ba8bb19
samarth <123548588+sam-3115@users.noreply.github.com>
2026-04-22 16:23:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ function buildGraph() {
   edges = new vis.DataSet();
   graph = {};
 
-  // Create nodes
   for (let i = 0; i < n; i++) {
     nodes.add({ id: i, label: i.toString() });
     graph[i] = [];
   }
 
-  // Create edges
   edgeInput.forEach(line => {
-    let [u, v] = line.split(" ").map(Number);
-    edges.add({ from: u, to: v });
-    graph[u].push(v);
-    graph[v].push(u); // undirected
+    let [u, v, w] = line.split(" ").map(Number);
+
+    edges.add({
+      from: u,
+      to: v,
+      label: w.toString()
+    });
+
+    graph[u].push([v, w]);
+    graph[v].push([u, w]); // undirected
   });
 
   let container = document.getElementById("network");
@@ -80,4 +84,43 @@ async function runDFS() {
   }
 
   await dfs(0);
+}
+async function runDijkstra() {
+  resetGraph();
+
+  let start = parseInt(document.getElementById("startNode").value);
+
+  let dist = {};
+  let visited = new Set();
+
+  for (let node in graph) dist[node] = Infinity;
+  dist[start] = 0;
+
+  while (true) {
+    let u = -1;
+
+    // find min distance node
+    for (let node in dist) {
+      if (!visited.has(node) && (u === -1 || dist[node] < dist[u])) {
+        u = node;
+      }
+    }
+
+    if (u === -1 || dist[u] === Infinity) break;
+
+    visited.add(u);
+
+    highlightNode(parseInt(u), "orange");
+    await sleep(800);
+
+    for (let [v, w] of graph[u]) {
+      if (dist[u] + w < dist[v]) {
+        dist[v] = dist[u] + w;
+      }
+    }
+
+    highlightNode(parseInt(u), "green");
+  }
+
+  console.log("Final distances:", dist);
 }
